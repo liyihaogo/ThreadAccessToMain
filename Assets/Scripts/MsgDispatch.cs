89a685c75a1c17c8f9da503ccccd1655f76ec366
liyihaogo <2640705340@qq.com>
2018-09-29 18:02:57 +0800

    first commit

---
+++ Assets/Scripts/MsgDispatch.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void MsgHandle(object obj);
+/// <summary>
+/// 消息分发及数据队列
+/// </summary>
+public class MsgDispatch
+{
+	private static MsgDispatch m_instance;
+	public static MsgDispatch MInstance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				m_instance = new MsgDispatch();
+			}
+			return m_instance;
+		}
+	}
+
+	private Queue<MsgData> m_msgData = new Queue<MsgData>();
+	private Dictionary<int, List<MsgHandle>> m_msgHandle = new Dictionary<int, List<MsgHandle>>();
+
+	public void RegMsg(int mid, MsgHandle msg)
+	{
+		List<MsgHandle> msgLst;
+		if (!m_msgHandle.TryGetValue(mid, out msgLst))
+		{
+			msgLst = new List<MsgHandle>();
+			m_msgHandle.Add(mid, msgLst);
+		}
+		msgLst.Add(msg);
+	}
+
+	public void UnRegMsg(int mid, MsgHandle msg)
+	{
+		List<MsgHandle> msgLst;
+		if (!m_msgHandle.TryGetValue(mid, out msgLst))
+		{
+			return;
+		}
+		msgLst.Remove(msg);
+		if (msgLst.Count <= 0)
+			m_msgHandle.Remove(mid);
+	}
+
+	public void Dispatch(int mid, object data)
+	{
+		List<MsgHandle> msgLst;
+		if (!m_msgHandle.TryGetValue(mid, out msgLst)) return;
+		int len = msgLst.Count;
+		for (int i = 0; i < len; i++)
+		{
+			msgLst[i](data);
+		}
+	}
+
+	public void Push(MsgData data)
+	{
+		m_msgData.Enqueue(data);
+	}
+
+	public void Update()
+	{
+		if (m_msgData.Count > 0)
+		{
+			MsgData mdata = m_msgData.Dequeue();
+			MsgDispatch.MInstance.Dispatch(mdata.MMsgId, mdata.MSpeed);
+		}
+	}
+}
+
+public class MsgData
+{
+	public int MMsgId = 100;
+	public float MSpeed = 0f;
+}
---
+++ Assets/Scripts/ThreadEntrance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+/// <summary>
+/// 子线程发送数据给主线程并对主线程创建的可见对象进行操作
+/// 主要通过把子线程创建的数据缓存到队列中，然后在主线程的update中把队列中的数据分发出去
+/// </summary>
+public class ThreadEntrance : MonoBehaviour
+{
+	public GameObject m_curGm;
+	private void Awake()
+	{
+		MsgDispatch.MInstance.RegMsg(100, MoveTarget);
+	}
+
+	private void MoveTarget(object obj)
+	{
+		m_curGm.transform.Rotate(0, (float)obj, 0);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		Thread thread = new Thread(new ThreadStart(MoveTarget));
+		thread.IsBackground = true;
+		thread.Start();
+	}
+
+	private void MoveTarget()
+	{
+		while (true)
+		{
+			MsgDispatch.MInstance.Push(new MsgData() { MMsgId = 100, MSpeed = 10});
+			Thread.Sleep(1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		MsgDispatch.MInstance.Update();
+	}
+}
